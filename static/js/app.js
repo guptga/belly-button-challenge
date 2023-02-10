761f1f684b795e9946977ec282ef0a1f935b3c51
guptga <116124534+guptga@users.noreply.github.com>
2023-02-09 23:32:14 -0500

    updated codes

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,4 @@
+// Place url in a constant variable
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
 // Fetch the JSON data and console log it
@@ -187,6 +188,7 @@ function optionChanged(value) {
 // Call the initialize function
 init();
 
+
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
     console.log(data);
@@ -289,4 +291,3 @@ d3.json(url).then(function(data) {
   
   // Call the initialize function
   init();
-  
